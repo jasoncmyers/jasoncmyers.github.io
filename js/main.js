3e1f7b78900d0a0a321c4dd2c87c128fbed7c68f
myer0154 <myers.jason.c@gmail.com>
2017-07-31 21:23:21 -0500

    Add first draft of portfolio: index.html, main.css, main.js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  
+  $("#navbar-about").click(function(event) {
+    event.preventDefault();
+    $(window).scrollTo($("#about"), 500);
+  });
+  
+  $("#navbar-portfolio").click(function(event) {
+    event.preventDefault();
+    $(window).scrollTo($("#portfolio"), 500);
+  });
+  
+  $("#navbar-contact").click(function(event) {
+    event.preventDefault();
+    $(window).scrollTo($("#contact"), 500);
+  });
+  
+  $(".navbar-brand").click(function(event) {
+    event.preventDefault();
+    $(window).scrollTo(0, 500);
+  })
+  
+})
